c3bd2b04d8c08d9597a6695424b671dba3d0356c
Atul <atulydv15@gmail.com>
2026-07-17 12:51:15 +0530

    Added Spring Core and Maven exercises

--- JUnitMockitoandSL4J/BasicTestingExercises/Exercise3/AssertionsTest.java
+++ JUnitMockitoandSL4J/BasicTestingExercises/Exercise3/AssertionsTest.java
@@ -8,19 +8,19 @@ public class AssertionsTest {
     @Test
     public void testAssertions() {
 
-        // Assert Equals
+
         assertEquals(5, 2 + 3);
 
-        // Assert True
+        
         assertTrue(10 > 5);
 
-        // Assert False
+        
         assertFalse(5 > 10);
 
-        // Assert Null
+    
         assertNull(null);
 
-        // Assert Not Null
+        
         assertNotNull(new Object());
     }
 }
